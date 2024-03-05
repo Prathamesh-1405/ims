9be8a11b97022c6f4360562e9d91c366792757fe
Prathamesh-1405 <prathameshpardhiye45@gmail.com>
2024-03-05 20:22:54 +0530

    Started creating sidebar

--- app/src/main/java/com/example/myapplication/HomeActivity.java
+++ app/src/main/java/com/example/myapplication/HomeActivity.java
@@ -1,15 +1,19 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-public class HomeActivity extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
 
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,4 +34,31 @@ public class HomeActivity extends AppCompatActivity {
     public void onClickNewOrder(View view) {
         startActivity(new Intent(HomeActivity.this,CatalogActivity.class));
     }
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.nav_home) {
+            // Handle home item click
+
+        } else if (itemId == R.id.nav_settings) {
+            // Handle settings item click
+
+        } else if (itemId == R.id.nav_share) {
+            // Handle share item click
+
+        } else if (itemId == R.id.nav_about) {
+            // Handle about item click
+
+        } else if (itemId == R.id.nav_logout) {
+            // Handle logout item click
+            Toast.makeText(this, "Logout!", Toast.LENGTH_SHORT).show();
+
+        }
+
+        // Close the drawer after handling the click
+        return true;
+    }
+
+
 }
